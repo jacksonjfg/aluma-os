ac15011fab7ae54d589f9e80c69f6aaac11305f1
jacksonjfg <jackson@alumamkt.com.br>
2026-06-24 16:06:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'aluma-os-v21';
+const CACHE_NAME = 'aluma-os-v32';
 const APP_SHELL = [
   './manifest.webmanifest',
   './icons/icon-192.png',
@@ -49,8 +49,3 @@ self.addEventListener('fetch', event => {
     );
   }
 });
-
-
-
-
-
